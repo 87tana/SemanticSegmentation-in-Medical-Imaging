be9f724b94c999df089e94080fcd6e2e71426c7b
Tannaz Mostafid <112851112+87tana@users.noreply.github.com>
2024-06-08 19:49:05 +0200

    Add files via upload

---
+++ src/resnet_batchnorm.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn  # Neural network modules and loss functions
+import torchvision.models as models # provide access to pre-trained models
+
+
+
+class ResNetBatch(nn.Module):
+    def __init__(self, num_classes=1, out_channels=1,):
+        super(ResNetBatch, self).__init__()
+
+        # Load pre-trained ResNet model
+        resnet = models.resnet18(pretrained=True)  # import torchvision
+
+        # Remove fully connected layers (classifier) from ResNet
+        self.encoder = nn.Sequential(*list(resnet.children())[:-2]) #remove two last layer,
+
+        # Decoder with transposed convolution
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, num_classes, kernel_size=1)
+        )
+
+
+        # Decoder with upsampling (interpolation)
+        self.decoder_up = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(512, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),   # added batchnorm to all layer in exp 13
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),  # add one conv layer in exp14
+            nn.BatchNorm2d(256), 
+            nn.ReLU(inplace=True),
+            
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128), 
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(128, 128, kernel_size=3, padding=1), # add one conv layer in exp15
+            nn.BatchNorm2d(128), 
+            nn.ReLU(inplace=True),
+            
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64), 
+            nn.ReLU(inplace=True),
+            
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),
+            #nn.BatchNorm2d(32), 
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, num_classes, kernel_size=1)
+            #nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        # Encoder
+        x = self.encoder(x)
+
+        # Decoder
+        x = self.decoder_up(x)
+        return x
