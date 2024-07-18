587c27378939f8c9c6381edcbc7d98e5ab3b7de4
Tannaz Mostafid <112851112+87tana@users.noreply.github.com>
2024-07-18 13:20:59 +0200

    Add files via upload

--- src/utils/transform.py
+++ src/utils/transform.py
@@ -1,9 +1,13 @@
 #import torchvision.transforms as transforms
-import torchvision.transforms.v2 as transforms
+from torchvision.transforms import v2
+import numpy as np
+import torch
+import random
+from scipy.ndimage import gaussian_filter, map_coordinates
+from PIL import Image
 
 
-
-class deafault_transform():
+class default_transform():
   def __init__(self, outputsize=None):
 
     self.outputsize=outputsize
@@ -11,15 +15,15 @@ class deafault_transform():
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
 
-    self.imgtrans = transforms.Compose([
-    transforms.ToTensor(), # converting image data from a numpy array or PIL image format to a PyTorch tensor format.
-    transforms.Normalize(mean=mean, std=std) # normalize the image
+    self.imgtrans = v2.Compose([
+    v2.ToTensor(), # converting image data from a numpy array or PIL image format to a PyTorch tensor format.
+    v2.Normalize(mean=mean, std=std) # normalize the image
     ])
 
     
-    self.masktrans = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Resize((self.outputsize, self.outputsize)), # convert mask to PyTorch tensor
+    self.masktrans = v2.Compose([
+        v2.ToTensor(),
+        v2.Resize((self.outputsize, self.outputsize)), # convert mask to PyTorch tensor
     ])
 
   def trans(self, img, mask):
@@ -31,27 +35,30 @@ class deafault_transform():
 
 
 
-class augmentation(deafault_transform):
+class augmentation(default_transform):
   def __init__(self, outputsize, auglist=None):
     super().__init__(outputsize)
   
-    self.affine = transforms.Compose([
-      transforms.RandomHorizontalFlip(p=0.5), # Randomly flip the image horizontally
-      transforms.RandomVerticalFlip(p=0.5), # Randomly flip the image vertically
-      transforms.RandomRotation(degrees=90), # Randomly rotate the image
-      ])
+    self.affine = v2.Compose([v2.RandomHorizontalFlip(p=0.5),
+                        v2.RandomVerticalFlip(p=0.5),
+                        v2.RandomAffine(degrees=(10, 70), translate=(0.2, 0.2), shear=(-10,10,-10,10), scale=(0.8, 1.2)),
+                        ])
 
-    self.imgaug =  transform_img = transforms.Compose([
-      transforms.ColorJitter(brightness=0.1), # Apply random brightness augmentation
-       ])
+    self.imgaug = v2.Compose([v2.ElasticTransform(alpha=10, sigma=0.08),
+                                v2.ColorJitter(brightness=(0.1,1.0), contrast=(0.7,1.0), saturation=(0.7,1.0)) # Apply random brightness augmentation
+                              ])
       
 
   def augment(self, img, mask):
+
+    img = Image.fromarray(img)
+    mask = Image.fromarray(mask)     
 
     img, mask = self.affine(img, mask)
     img = self.imgaug(img)
 
     img, mask = self.trans(img, mask)
+    
 
 
     return img, mask
@@ -78,3 +85,7 @@ class augmentation(deafault_transform):
       #transforms.ToTensor(), # Convert mask to a PyTorch tensor
       #transforms.Resize((320, 320)), # Resize the mask
   #])
+
+
+
+
